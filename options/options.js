ef9dbd4221b3b4d4eae0044403bf428e756a6db4
Crowdbotics <team@crowdbotics.com>
2023-08-28 13:09:12 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "billowing-flower-43118",
+  url: "https://billowing-flower-43118.botics.co",
+  api: "https://billowing-flower-43118.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
